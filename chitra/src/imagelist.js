39e96fddc4bbab904d9dc2d09fff2af5827b9a77
SidWorks <51191127+SidWorks@users.noreply.github.com>
2020-04-21 12:07:31 +0530

    initial files

---
+++ chitra/src/imagelist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class imageList extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default imageList;
---
+++ chitra/src/searchbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class SearchBar extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default SearchBar;
