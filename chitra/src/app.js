666ecfc68eba704d5777330ba512d0c8aba64c20
SidWorks <51191127+SidWorks@users.noreply.github.com>
2020-04-21 12:05:33 +0530

    initial files

---
+++ chitra/src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Helloo There !</h1>
+        <h3>We are starting the project now!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
