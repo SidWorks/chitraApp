4784e6f1207eaf38eef36272fcbb0b5f27ec3436
SidWorks <51191127+SidWorks@users.noreply.github.com>
2020-04-22 15:00:43 +0530

    initial files

--- chitra/src/searchbar.js
+++ chitra/src/searchbar.js
@@ -1,19 +1,31 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = {};
+  state = { type: "" };
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.type);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" action="post">
-          <button type="submit">Image Search</button>
+        <form onSubmit={this.onFormSubmit} className="ui form" action="post">
+          <label htmlFor="">
+            <b>Image Search</b>
+          </label>
 
-          <input className="field" type="text" onChange={this.onInputChange} />
+          <input
+            value={this.state.type}
+            className="field"
+            type="text"
+            onChange={(e) =>
+              this.setState({
+                type: e.target.value.toUpperCase(),
+              })
+            }
+          />
         </form>
       </div>
     );
