8dd52a3350c76d73bbc926999d62cd611adda643
SidWorks <51191127+SidWorks@users.noreply.github.com>
2020-04-28 17:29:34 +0530

    css added

--- chitra/src/components/imagelist.js
+++ chitra/src/components/imagelist.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./imageList.css";
+import ImageCard from "./imageCard";
 
 const ImageList = (props) => {
   console.log(props.images);
   const images = props.images.map((image) => {
-    return (
-      <img
-        alt={image.alt_description}
-        key={image.id}
-        src={image.urls.regular}
-      />
-    );
+    return <ImageCard image={image} key={image.id} />;
   });
   return <div className="image-list">{images}</div>;
 };
